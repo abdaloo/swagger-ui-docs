a1550888c80c5c53c916abfec207122067006246
Abdal-Hussain <132570573+abdaloo@users.noreply.github.com>
2025-08-04 15:45:20 +0500

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://school-management-system-eb-backend.vercel.app/swagger.json",
+    url: "https://forexample/swagger.json",//put your own link here of vercel or anyother deployment link
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
